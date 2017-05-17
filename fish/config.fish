37ad3331f7cdb252b1823cb2d8f5c23d4b86d91b
km-tr <kinisinakuteiiyo@gmail.com>
2017-05-17 15:05:42 +0900

    Update

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,7 @@
 # theme-bobthefish
 set -g theme_color_scheme terminal2-dark
 set -g theme_display_git yes
-set -g theme_display_git_untracked yes
-set -g theme_display_git_ahead_verbose yes
-set -g theme_git_worktree_support yes
-set -g theme_display_date no
-set -g theme_display_cmd_duration yes
+set -g theme_display_ruby no
+set -g theme_git_worktree_support yes 
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_project_dir_length 1
