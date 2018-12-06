2d6a87d6e5f447da5ad2522384483b50587067c2
km-tr <kumagai@bear.tokyo>
2018-12-06 09:34:20 +0900

    update fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,1 @@
-# theme-bobthefish
-set -g theme_color_scheme terminal2-dark
-set -g theme_display_git yes
-set -g theme_display_ruby no
-set -g theme_git_worktree_support yes 
-set -g fish_prompt_pwd_dir_length 1
-set -g theme_project_dir_length 1
+../.config/fish/config.fish
