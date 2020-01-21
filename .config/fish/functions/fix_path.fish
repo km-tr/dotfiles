e7ff9a3ad47c412e56732d02b2360af1a539d0ea
km-tr <kumagai@bear.tokyo>
2020-01-21 14:10:16 +0900

    echoを削除

--- .config/fish/functions/fix_path.fish
+++ .config/fish/functions/fix_path.fish
@@ -1,8 +1,6 @@
 function fix_path --description "Removes duplicate entries from \$PATH"
   set -l NEWPATH
   for p in $PATH
-    echo $p
-    echo $NEWPATH
     if not contains $p $NEWPATH
       set NEWPATH $NEWPATH $p
     end
