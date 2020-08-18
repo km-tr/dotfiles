f269fd8f8d6f521eed64a9da6e9dcb0c6eb2164e
km-tr <kumagai@bear.tokyo>
2020-08-18 23:20:25 +0900

    update nodenv

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -3,12 +3,12 @@ if status --is-interactive
 
   # direnv
   eval (direnv hook fish)
+  eval (nodenv init - | source)
 
   # android studio
   set -x ANDROID_HOME $HOME/Library/Android/sdk
 
   # PATH
-  set PATH $HOME/.nodenv/bin $PATH
   set PATH $HOME/.rbenv/bin $PATH
   set PATH $HOME/.pyenv/bin $PATH
 end
