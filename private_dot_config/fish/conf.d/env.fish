bbe2c97177393c29f64a9ffbea15c977dcaf1939
km-tr <kumagai@bear.tokyo>
2022-02-28 12:54:17 +0900

    Add private_dot_config/fish/conf.d/env.fish

---
+++ private_dot_config/fish/conf.d/env.fish
@@ -0,0 +1,21 @@
+if status --is-interactive
+  set -x EDITOR vim
+
+  # direnv
+  eval (direnv hook fish)
+  eval (nodenv init - | source)
+  eval (rbenv init - | source)
+  eval (goenv init - | source)
+
+  # android studio
+  set -x ANDROID_HOME $HOME/Library/Android/sdk
+  
+  # zoxide
+  zoxide init fish | source
+
+  # PATH
+  set PATH $HOME/.pyenv/bin $PATH
+
+  # rust
+  
+end
