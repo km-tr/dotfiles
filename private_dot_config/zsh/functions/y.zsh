c3707c5747ae62e3ef70c2b91138f90e4dfa27d6
km-tr <kumagai@bear.tokyo>
2026-02-17 18:21:53 +0900

    chore: zsh functions

---
+++ private_dot_config/zsh/functions/y.zsh
@@ -0,0 +1,7 @@
+y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	command yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
